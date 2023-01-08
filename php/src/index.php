c7ad0ea5224a4abdb2d05ccc6659e681077eebe8
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-08 10:30:49 -0600

    added conditional to check that value has been submitted each turn and to check that value is only x or o.

--- php/src/index.php
+++ php/src/index.php
@@ -6,6 +6,8 @@
     <h2>PHP Tic Tac Toe</h2>
     <form method="POST" action="index.php">
         <?php
+
+        $error=false;
             // create 9 boxes for input fields (3x3) using for loop
             for($id=1; $id<=9; $id+=1) {
                 // insert break at 4 and 7 to make three rows
@@ -13,6 +15,16 @@
                      echo "<br />";
                 }
                 echo "<input name=$id type=text size=7>";
+                // determine when value has been submitted and is not empty
+                if(isset($_POST['submit']) && !empty($_POST[$id])) {
+                    // ensure value printed is only 'x' or 'p'
+                    if(strtolower($_POST[$id]) === "x" | strtolower($_POST[$id]) === "o") {
+                        echo "value=$_POST[$id]";
+                    } else {
+                        $error=true;
+                    }
+
+                }
             }
         ?>
     </form>
