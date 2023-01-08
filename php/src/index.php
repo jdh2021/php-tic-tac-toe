ad2cac2407fbf7212f164625d361b49e5204bd97
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-08 11:22:12 -0600

    added for loop and conditional to check for matches at fields 147, 258, and 369. added for loop and conditional to check for diagonal matches at fields 159 and 357.

--- php/src/index.php
+++ php/src/index.php
@@ -30,6 +30,26 @@
                                 }
                             }
                         }
+                        // check for matches down columns at 147, 258, 369
+                        for($a=1, $b=4, $c=7; $a<=3, $b<=6, $c<=9; $a+=1, $b+=1, $c+=1) {
+                            if($_POST[$a]===$_POST[$b] && $_POST[$b]===$_POST[$c]) {
+                                if($_POST[$a]==="x") {
+                                    $player_x_wins = true;
+                                } elseif ($_POST[$a] ==="o") {
+                                    $player_o_wins = true;
+                                }
+                            }
+                        }
+                        // check for diagonal matches at 159, 357
+                        for($a=1, $b=5, $c=9; $a<=3, $b<=5, $c<=9; $a+=2, $b+=0, $c-=2) {
+                            if($_POST[$a]===$_POST[$b] && $_POST[$b]===$_POST[$c]) {
+                                if($_POST[$a]==="x") {
+                                    $player_x_wins = true;
+                                } elseif ($_POST[$a] ==="o") {
+                                    $player_o_wins = true;
+                                }
+                            }
+                        }
                     } else {
                         $error=true;
                     }
