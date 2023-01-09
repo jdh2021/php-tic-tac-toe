c1eaeb348edb8ddfeddd8253cd5020739cf98164
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-09 14:32:53 -0600

    troubleshooted input count and value error. moved input count within conditional that first checks if input is x or o. updated spacing for input value.

--- php/src/index.php
+++ php/src/index.php
@@ -18,27 +18,27 @@
                 if ($id === 4 or $id === 7) {
                      echo "<br />";
                 }
-                echo "<input name=$id type=text size=7>";
+                echo "<input name=$id type=text size=7";
                 // determine when value has been submitted and is not empty
                 if(isset($_POST['submit']) && !empty($_POST[$id])) {
-                    // add 1 to input count each time a value is submitted. max input count is 9. 
-                    $input_count+=1;
                     // ensure value printed is only 'x' or 'o'
-                    if(strtolower($_POST[$id]) === "x" | strtolower($_POST[$id]) === "o") {
-                        echo "value=$_POST[$id]";
+                    if((strtolower($_POST[$id]) === "x") | (strtolower($_POST[$id]) === "o")) {
+                        // add 1 to input count each time a value is submitted. max input count is 9. 
+                        $input_count+=1;
+                        echo " value =".$_POST[$id]." readonly>";
                         // check for matches across rows at inputs 123, 456, 789
                         for($a=1, $b=2, $c=3; $a<=7, $b<=8, $c<=9; $a+=3, $b+=3, $c+=3) {
-                            if($_POST[$a]===$_POST[$b] && $_POST[$b]===$_POST[$c]) {
+                            if(($_POST[$a]===$_POST[$b]) && ($_POST[$b]===$_POST[$c])) {
                                 if($_POST[$a]==="x") {
                                     $player_x_wins = true;
-                                } elseif ($_POST[$a] ==="o") {
+                                } elseif ($_POST[$a]==="o") {
                                     $player_o_wins = true;
                                 }
                             }
                         }
                         // check for matches down columns at 147, 258, 369
                         for($a=1, $b=4, $c=7; $a<=3, $b<=6, $c<=9; $a+=1, $b+=1, $c+=1) {
-                            if($_POST[$a]===$_POST[$b] && $_POST[$b]===$_POST[$c]) {
+                            if(($_POST[$a]===$_POST[$b]) && ($_POST[$b]===$_POST[$c])) {
                                 if($_POST[$a]==="x") {
                                     $player_x_wins = true;
                                 } elseif ($_POST[$a] ==="o") {
@@ -47,8 +47,8 @@
                             }
                         }
                         // check for diagonal matches at 159, 357
-                        for($a=1, $b=5, $c=9; $a<=3, $b<=5, $c<=9; $a+=2, $b+=0, $c-=2) {
-                            if($_POST[$a]===$_POST[$b] && $_POST[$b]===$_POST[$c]) {
+                        for($a=1, $b=5, $c=9; $a<=3, $b<=5, $c<=7; $a+=2, $b+=0, $c-=2) {
+                            if(($_POST[$a]===$_POST[$b]) && ($_POST[$b]===$_POST[$c])) {
                                 if($_POST[$a]==="x") {
                                     $player_x_wins = true;
                                 } elseif ($_POST[$a] ==="o") {
@@ -59,7 +59,9 @@
                     } else {
                         $error=true;
                     }
-
+                }
+                else {
+                    echo ">";
                 }
             }
         ?>
