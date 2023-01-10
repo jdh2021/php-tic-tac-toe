fbc94c5d4630f7f783a14f50c1da56f3e58b3a0f
Jennifer Hightower <jenniferhightower2020@gmail.com>
2023-01-10 12:33:11 -0600

    revised strtolower to ensure all capitalized Xs and Os would be recognized as matches. created Restart Game button that conditionally renders when there is a win or draw

--- php/src/index.php
+++ php/src/index.php
@@ -11,48 +11,49 @@
         $player_x_wins=false;
         $player_o_wins=false;
         $input_count=0;
+        $character_entered=null;
 
             // create 9 boxes for input fields (3x3) using for loop
             for($id=1; $id<=9; $id+=1) {
-                // insert break at 4 and 7 to make three rows
+                // insert break prior 4 and 7 to make three rows
                 if ($id === 4 or $id === 7) {
                      echo "<br />";
                 }
                 echo "<input name=$id type=text size=7";
                 // determine when value has been submitted and is not empty
                 if(isset($_POST['submit']) && !empty($_POST[$id])) {
-                    $input_character = strtolower($_POST[$id]);
                     // ensure value printed is only 'x' or 'o'
+                    $input_character = strtolower($_POST[$id]);
                     if($input_character === "x" | $input_character === "o") {
                         // add 1 to input count each time a value is submitted. max input count is 9. 
                         $input_count+=1;
                         echo " value=".$input_character." readonly>";
                         // check for matches across rows at inputs 123, 456, 789
                         for($a=1, $b=2, $c=3; $a<=7, $b<=8, $c<=9; $a+=3, $b+=3, $c+=3) {
-                            if(($_POST[$a]===$_POST[$b]) && ($_POST[$b]===$_POST[$c])) {
-                                if($_POST[$a]==="x") {
+                            if((strtolower($_POST[$a])===strtolower($_POST[$b]) && strtolower($_POST[$b])===strtolower($_POST[$c]))) {
+                                if(strtolower($_POST[$a])==="x") {
                                     $player_x_wins = true;
-                                } elseif ($_POST[$a]==="o") {
+                                } elseif (strtolower($_POST[$a])==="o") {
                                     $player_o_wins = true;
                                 }
                             }
                         }
                         // check for matches down columns at 147, 258, 369
                         for($a=1, $b=4, $c=7; $a<=3, $b<=6, $c<=9; $a+=1, $b+=1, $c+=1) {
-                            if(($_POST[$a]===$_POST[$b]) && ($_POST[$b]===$_POST[$c])) {
-                                if($_POST[$a]==="x") {
+                            if((strtolower($_POST[$a])===strtolower($_POST[$b]) && strtolower($_POST[$b])===strtolower($_POST[$c]))) {
+                                if(strtolower($_POST[$a])==="x") {
                                     $player_x_wins = true;
-                                } elseif ($_POST[$a] ==="o") {
+                                } elseif (strtolower($_POST[$a])==="o") {
                                     $player_o_wins = true;
                                 }
                             }
                         }
                         // check for diagonal matches at 159, 357
                         for($a=1, $b=5, $c=9; $a<=3, $b<=5, $c>=7; $a+=2, $b+=0, $c-=2) {
-                            if(($_POST[$a]===$_POST[$b]) && ($_POST[$b]===$_POST[$c])) {
-                                if($_POST[$a]==="x") {
+                            if((strtolower($_POST[$a])===strtolower($_POST[$b]) && strtolower($_POST[$b])===strtolower($_POST[$c]))) {
+                                if(strtolower($_POST[$a])==="x") {
                                     $player_x_wins = true;
-                                } elseif ($_POST[$a] ==="o") {
+                                } elseif (strtolower($_POST[$a])==="o") {
                                     $player_o_wins = true;
                                 }
                             }
@@ -69,22 +70,25 @@
         ?>
         <br />
         <br />
-        <button name="submit" type="submit">Complete Turn</button>
-    </form>
-    <?php 
+        <?php 
         if ($player_x_wins) {
-            echo "Player x won!";
+            echo "<button name=start type=submit>Restart Game</button><br /><br />";
+            echo "Player X won!";
         } elseif($player_o_wins) {
-            echo "Player o won!";
+            echo "<button name=start type=submit>Restart Game</button><br /><br />";
+            echo "Player O won!";
         } elseif($input_count===9 && !$player_x_wins && !$player_o_wins ) {
+            echo "<button name=start type=submit>Restart Game</button><br /><br />";
             echo "It's a draw!";
         } elseif($error) {
+            echo "<button name=submit type=submit>Complete Turn</button><br /><br />";
             echo "Please only enter 'x' or 'o'!";
         } else {
+            echo "<button name=submit type=submit>Complete Turn</button><br /><br />";
             echo "Enter 'x' or 'o' and then click 'Complete Turn'";
         }
-    ?>
-
+    ?>    
+    </form>
 </body>
 
 </html>
